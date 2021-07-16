a899595c1f9384afa661dd24919bf7bb38e069ab
Modelka <patrycja.modelska@onet.pl>
2021-07-16 10:13:00 +0200

    and setup and teardown

--- functional_tests.py
+++ functional_tests.py
@@ -3,7 +3,20 @@ from selenium import webdriver
 
 
 class DjangoReadyToWorkTestCase(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+        self.browser.get('http://127.0.0.1:8000')
+
     def test_check_browser_title(self):
-        browser = webdriver.Firefox()
-        browser.get('http://127.0.0.1:8000')
-        self.assertEqual(browser.title, 'The install worked successfully! Congratulations!')
+        self.assertEqual(self.browser.title, 'The install worked successfully! Congratulations!')
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+         self.assertIn('Listy', self.browser.title)
+         self.fail('Zakończenie testu!')
+
+    def tearDown(self):
+        self.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
