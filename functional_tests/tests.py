ad2db537b5ea28bf6cf9304960d26c8e439bb23e
Modelka <patrycja.modelska@onet.pl>
2021-07-16 19:19:14 +0200

    New URL, view and template for display list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.assertIn('Listy', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('Twoja lista rzeczy do zrobienia', header_text)
+        self.assertIn('Utwórz nową listę rzeczy do zrobienia', header_text)
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
@@ -31,6 +31,13 @@ class NewVisitorTest(LiveServerTestCase):
         )
         inputbox.send_keys('Kupić pawie pióra')
         inputbox.send_keys(Keys.ENTER)
+
+        import time
+        time.sleep(2)
+
+        edith_list_url = self.browser.current_url
+        print(edith_list_url)
+        self.assertRegex(edith_list_url, '/lists/.+')
 
         import time
         time.sleep(6)
@@ -47,7 +54,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table('1: Kupić pawie pióra')
         self.check_for_row_in_list_table('2: Użyć pawich piór do zrobienia przynęty')
 
-        self.fail('Zakończenie testu!')
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+        # Franek odwiedza stronę główną.
+
+        # Nie znajduje żadnych śladów listy Edyty.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Kupić pawie pióra', page_text)
+        self.assertNotIn('zrobienia przynęty', page_text)
+        # Franek tworzy nową listę, wprowadzając nowy element.
+        # Jego lista jest mniej interesująca niż Edyty…
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Kupić mleko')
+        inputbox.send_keys(Keys.ENTER)
+        # Franek otrzymuje unikatowy adres URL prowadzący do listy.
+
+        time.sleep(3)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        # Ponownie nie ma żadnego śladu po liście Edyty.
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Kupić pawie pióra', page_text)
+        self.assertIn('Kupić mleko', page_text)
+        # Usatysfakcjonowani, oboje kładą się spać.
 
 
 
--- lists/urls.py
+++ lists/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, view_list
 
 urlpatterns = [
     path('', home_page, name='home'),
+    path('lists/the-only-list-in-the-world/', view_list, name='view list')
 ]
