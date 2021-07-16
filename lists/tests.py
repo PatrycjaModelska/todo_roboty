a07b9706fd76179f61c9a42905b4959aeece786a
Modelka <patrycja.modelska@onet.pl>
2021-07-16 10:50:33 +0200

    Url mapping and add empty home_page view

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,9 @@
+from django.urls import resolve, reverse
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        url = reverse('home')
+        self.assertEqual(resolve(url).func, home_page)
