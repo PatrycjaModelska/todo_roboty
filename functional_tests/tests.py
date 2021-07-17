81df73545e4f32d7bf3e19ff92df10ee5c123e9d
Modelka <patrycja.modelska@onet.pl>
2021-07-17 12:35:27 +0200

    Refactorization of templates, base.html created

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -75,12 +75,21 @@ class NewVisitorTest(LiveServerTestCase):
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, '/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
-        # Ponownie nie ma żadnego śladu po liście Edyty.
+
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Kupić pawie pióra', page_text)
         self.assertIn('Kupić mleko', page_text)
-        # Usatysfakcjonowani, oboje kładą się spać.
 
+    def test_layout_and_styling(self):
+        # Edyta przeszła na stronę główną.
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        # Zauważyła, że pole tekstowe zostało elegancko wyśrodkowane.
 
-
-
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
+        # Edyta utworzyła nową listę i zobaczyła,
+        # że pole tekstowe nadal jest wyśrodkowane.
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
