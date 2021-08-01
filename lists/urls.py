d6ffe9c265698ee4ef6a2829175a133dda67209a
Modelka <patrycja.modelska@onet.pl>
2021-08-01 14:05:37 +0200

    Refactor hard-code URLs

--- lists/urls.py
+++ lists/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import view_list, new_list
 
 urlpatterns = [
-    path('<list_id>/', view_list, name='view list'),
-    path('new', new_list, name='new list'),
+    path('<list_id>/', view_list, name='view_list'),
+    path('new', new_list, name='new_list'),
 ]
